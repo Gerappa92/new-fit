606e4e0af7522b94a3a676ecec3d320689bdd57c
Krzysztof Juszcze <krzjus@gmail.com>
2020-08-18 22:38:37 +0200

    Updates

---
+++ precache-manifest.b804d0beb898aae8d6d1fedf7fd6f3b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bef9f7ec3855d2ff4111f3eea47b4a7",
+    "url": "/new-fit/index.html"
+  },
+  {
+    "revision": "0e53f2c2093f701e1b0d",
+    "url": "/new-fit/static/css/main.8c11f739.chunk.css"
+  },
+  {
+    "revision": "89b56a53213722c1fb5d",
+    "url": "/new-fit/static/js/2.b29b3adf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/new-fit/static/js/2.b29b3adf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e53f2c2093f701e1b0d",
+    "url": "/new-fit/static/js/main.6b06a03d.chunk.js"
+  },
+  {
+    "revision": "4399edabbc65df0a973a",
+    "url": "/new-fit/static/js/runtime-main.09186688.js"
+  },
+  {
+    "revision": "ebfce459fa9a96db91f7f054b4d7981e",
+    "url": "/new-fit/static/media/Bramka.ebfce459.PNG"
+  },
+  {
+    "revision": "8b08b9363a3a1c346adbb3309eef844c",
+    "url": "/new-fit/static/media/NEW_FIT_NASZYWKA_7x57cm.8b08b936.svg"
+  }
+]);
